208b8f33421ab7b11b47ae9b49c4b3f6d7d91e37
Jorge Lara <jlara@iee.unsj.edu.ar>
2022-11-28 16:07:11 -0300

    add files

--- main_openpy_fx_tools_dss.py
+++ main_openpy_fx_tools_dss.py
@@ -3,19 +3,12 @@ import openpy_fx_tools_dss as fx_dss
 xlsx = fx_dss.xlsx_DSS_xlsx()
 test = fx_dss.examples_lib()
 
-run_temp_xlsx = 0
-run_DSS_to_xlsx = 1
-run_xlsx_to_DSS = 1
+run_temp_xlsx = 1
+run_DSS_to_xlsx = 0
+run_xlsx_to_DSS = 0
 opt_crt = 1
 
 if __name__ == '__main__':
-    # dict_xlsx = fx_dss.load_examples_xlsx(5)
-    # xlsx_data = xlsx.load_examples_xlsx(1)  # Loads the examples loaded in the library
-    # xlsx.create_template_xlsx() # Generates the xlsx template for xlsx_data entry. In development
-    # xlsx.xlsx_to_OpenDSS(xlsx_path=xlsx_data['xlsx_path'], path_save=xlsx_data['path_save']) # Generate OpenDSS files.
-
-    # xlsx.create_template_xlsx()
-
     # Loads the examples loaded in the library
     DSS_path = test.load_examples_DSS(opt_crt, 'xlsx')
     xlsx_path = test.load_examples_xlsx(opt_crt)
